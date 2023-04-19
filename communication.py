a6b598f8e8519b3dc0010c4a27efe42a33d11adc
bschultz99 <bryantschultz99@gmail.com>
2023-04-19 16:29:20 -0500

    Docker testing

--- communication.py
+++ communication.py
@@ -3,19 +3,19 @@ from planetmint_driver.crypto import generate_keypair
 from ipld import marshal, multihash
 import time
 plntmnt = Planetmint('https://test.ipdb.io')
-
+#plntmnt = Planetmint('http://localhost:9984')
 def createPatient(name, dob, id, age=None, weight=None, height=None, gender=None, conditions=None): 
-    fixed_data = {
-        'data': {
+    fixed_data = [{
+        'data': multihash(marshal({
             'patient': {
                 'name': name,
                 'dob': dob,
                 'id': id,
                },
-           },
-       }
+           }))
+       }]
 
-    meta_data = {
+    meta_data = multihash(marshal({
         'data': {
             'patient': {
                 'age': age,
@@ -24,17 +24,17 @@ def createPatient(name, dob, id, age=None, weight=None, height=None, gender=None
                 'gender': gender,
                 'conditions': conditions,
             },
-        },
-    }
+        }
+    }))
     patient = generate_keypair()
     tx = plntmnt.transactions.prepare(
         operation = 'CREATE',
-        singers = patient.public_key,
+        signers = patient.public_key,
         assets = fixed_data,
-        metadata = meta_data, 
+        metadata = meta_data
     )
-    signed_tx = plntmnt.transactions.fulfill(tx, private_key=patient.private_key)
-    plntmnt.transactions.send_commit(signed_tx)
+    signed_tx = plntmnt.transactions.fulfill(tx, private_keys=patient.private_key)
+    print(plntmnt.transactions.send_commit(signed_tx))
     txid = signed_tx['id']
     return patient, txid
 
@@ -52,9 +52,11 @@ def updatePatient(patient, txid, age=None, weight=None, height=None, gender=None
     }
     tx = plntmnt.transactions.prepare(
         operation = 'UPDATE',
-        singers = patient.public_key,
+        signers = patient.public_key,
         asset = txid,
         metadata = meta_data,
     )
     signed_tx = plntmnt.transactions.fulfill(tx, private_key=patient.private_key)
     plntmnt.transactions.send_commit(signed_tx)
+
+print(createPatient('bob', '09/20/45', 'asd123', 56, 186, 156, "Male", "Cancer"))
